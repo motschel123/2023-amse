53436c6c21f36202665a13adb93226a1a2286a83
Marcel Schöckel <marcel.schoeckel@gmail.com>
2023-06-13 18:15:22 +0200

    modify test, fix pipeline

--- project/test.sh
+++ project/test.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
-
-cd "$(git rev-parse --show-toplevel)"/data
+if [ "$current_dir" == "project" ]
+then
+    # if current directory is 'project', go to '../data'
+    cd ../data
+else
+    # if not, go to './data'
+    cd ./data
 pytest test.py
